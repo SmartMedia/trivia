73de9a17ede3989da36fa494ae1f098257aff068
Vít Krchov <vitkrchov@gmail.com>
2013-03-11 14:23:04 +0100

    All tests passed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,10 +6,14 @@ require 'active_record'
 
 require 'trivia'
 
+require 'tzinfo'
+
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
-  
+  config.before(:all) do
+    Time.zone = TZInfo::Timezone.get('Europe/Prague')
+  end
 end
